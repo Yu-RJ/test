3796a4f25389d5b2cb80609ead3cdf3ca29e8c08
YuRunJie <1228480306@qq.com>
2023-01-27 20:11:52 +0800

    v1.3.1

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <errno.h> //与全局变量errno相关的定义
 #include <fcntl.h> //定义了open、creat等函数，以及表示文件权限的宏定义
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h> //定义了ioctl函数
 #include <sys/types.h> //定义了一些常用数据类型，比如size_t
@@ -8,7 +9,7 @@
 
 int main(void) {
 
-    int fd = -1;
+    FILE *fp = NULL;
     int res = 0;
     int num = 8000000; // 8000000 = 1GB
 
@@ -21,70 +22,59 @@ int main(void) {
         "World!\nabcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMN";
     char read_buf[256] = {0};
 
-    fd = open(filename, "wb");
-    if (fd < 0) {
+    fp = fopen(filename, "wb");
+    if (!fp) {
         printf("%s file open fail", filename);
         return -1;
     }
     for (int i = 0; i < num; i++)
-        res = write(fd, write_dat, sizeof(write_dat));
-    if (res < 0) {
-        printf("write dat failb");
-        return -1;
-    } else
-        printf("write over ,num=%d\n", num);
-    close(fd);
+        fwrite(write_dat, sizeof(write_dat), 1, fp);
+    printf("write over ,num=%d\n", num);
+    fclose(fp);
 
+    // test 1
     printf("\n [test 1]: ... ... ... \n");
-    fd = open(filename, "rb");
-    if (fd < 0) {
+    fp = fopen(filename, "rb");
+    if (!fp) {
         printf("%s file open fail", filename);
         return -1;
     }
     for (int i = 0; i < num; i++) {
         for (int j = 0; j < 256; j++)
             read_buf[j] = '\0';
-        res = read(fd, read_buf, sizeof(write_dat));
+        fread(read_buf, sizeof(write_dat), 1, fp);
         if (strcmp(read_buf, write_dat) != 0) {
             printf("CMP ERROR: i= %d , read: %s \n", i, read_buf);
-            close(fd);
+            fclose(fp);
             return 0;
         }
     }
-    if (res < 0) {
-        printf("read dat fail");
-        return -1;
-    } else
-        printf("read over ,num=%d\n", num);
-    close(fd);
+    printf("read over ,num=%d\n", num);
+    fclose(fp);
     printf("[test 1]: PSASS \n");
 
     // test 2
     printf("\n [test 2]: ... ... ... \n");
-    fd = open(filename, "rb");
-    if (fd < 0) {
+    fp = fopen(filename, "rb");
+    if (!fp) {
         printf("%s file open fail", filename);
         return -1;
     }
     for (int j = 0; j < 256; j++)
         read_buf[j] = '\0';
-    res = read(fd, read_buf, sizeof(write_dat_miss_order_1));
+    fread(read_buf, sizeof(write_dat_miss_order_1), 1, fp);        
     for (int i = 0; i < num - 1; i++) {
         for (int j = 0; j < 256; j++)
             read_buf[j] = '\0';
-        res = read(fd, read_buf, sizeof(write_dat_miss_order_2));
+        fread(read_buf, sizeof(write_dat_miss_order_2), 1, fp); 
         if (strcmp(read_buf, write_dat_miss_order_2) != 0) {
             printf("CMP ERROR: i= %d , read: %s \n", i, read_buf);
-            close(fd);
+            fclose(fd);
             return 0;
         }
     }
-    if (res < 0) {
-        printf("read dat fail");
-        return -1;
-    } else
-        printf("read over ,num=%d\n", num);
-    close(fd);
+    printf("read over ,num=%d\n", num);
+    fclose(fp);
     printf("[test 2]: PSASS \n");
 
     return 0;
