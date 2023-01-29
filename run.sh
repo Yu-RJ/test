de08f82227637fb321dc99379de69356fc692568
YuRunJie <1228480306@qq.com>
2023-01-29 11:07:22 +0800

    v1.4.6

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
+echo "** creat ssd space :"
+sudo rm -rf ~/ssd
+mkdir ~/ssd
 sudo mkfs -t ext4 /dev/nvme0n1 > ~/log/testlog
 sudo mount /dev/nvme0n1 ~/ssd
+echo " PASS \n"
 echo "** git pull :"
 git pull origin master
 echo " PASS \n"
